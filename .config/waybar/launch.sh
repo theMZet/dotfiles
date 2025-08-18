7aef3eaf1d121467888453e1e38ac56789401bb6
theMZet <paszkietpawel@wp.pl>
2025-08-18 21:53:23 +0200

    Added Welcome to the game theme to waybar!

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -2,4 +2,4 @@
 
 pkill waybar
 
-waybar -c $HOME/.config/waybar/$1.jsonc -s $HOME/.config/waybar/$1.css > /dev/zero 2> /dev/zero &
+waybar -c $HOME/.config/waybar/$1.jsonc -s $HOME/.config/waybar/$1.css > /dev/zero &
