1d3d6f6c4514d50274205b49ba083f31bcf68e4e
theMZet <paszkietpawel@wp.pl>
2025-11-21 08:56:36 +0100

    feat: Changed margin from 10 to 20.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 20
 
 vim.cmd("set number")
 vim.cmd("set relativenumber")
