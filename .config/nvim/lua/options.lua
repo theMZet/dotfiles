e637fbf888dde3b713cf2b98d0a0a1a89070c747
theMZet <paszkietpawel@wp.pl>
2025-11-21 08:42:05 +0100

    feat: Added margin for bottom and top in nvim.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.opt.scrolloff = 10
 
 vim.cmd("set number")
 vim.cmd("set relativenumber")
